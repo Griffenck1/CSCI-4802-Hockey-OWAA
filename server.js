75c1986ef037918c7b15a7685a8256be5c54f517
Griffenck1 <griffenck1@gmail.com>
2021-05-02 14:27:43 -0600

    doing some final cleanup

--- server.js
+++ server.js
@@ -16,32 +16,6 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));
 
-//Database connection
-const pool = new Pool({
-  user: "bhfalumqxfcmdc",
-  host: "ec2-54-164-241-193.compute-1.amazonaws.com",
-  database: "dddq3k14glmlgd",
-  password: "ae370308a6cd30cea0f894205d904bc3ebddc0cb5b1de5b1bf7b59f3b3483d32",
-  port: "5432"
-});
-
-console.log("WTF");
-const query = {
-  text: "SELECT * from Team"
-}
-// callback
-pool.query(query, (err, res) => {
-  if (err) {
-    console.log(err.stack)
-  } else {
-    console.log(res.rows[0])
-  }
-});
-// promise
-pool
-  .query(query)
-  .then(res => console.log(res.rows[0]))
-  .catch(e => console.error(e.stack)) 
 /*
 Index/ Maps page
 */
@@ -63,15 +37,8 @@ app.get('/documentation', function(req, res) {
     })
 });
 
-
-
-
-
-
-
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
 	console.log(`Express running → PORT ${server.address().port}`);
 });
-
